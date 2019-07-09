2f8dedf59c277774ab919621df500a72e80dbea2
20bbrown14 <20bbrown14@gmail.com>
2019-07-09 09:56:02 -0500

    fix undefined error

--- commands/giphy.py
+++ commands/giphy.py
@@ -1,5 +1,6 @@
 async def giphy_command(client, message, response_channel, delete_message, giphy_api_key):
   forbidden_gifs = ['/gamerescape', '/xivdb', '/giphy', '/tts', '/tenor', '/me', '/tableflip', '/unflip', '/shrug', '/nick']
+  messageContent = message.content
   spaceIndex = messageContent.find(' ')
   if spaceIndex != -1 and messageContent[:spaceIndex] in forbidden_gifs:
     return
